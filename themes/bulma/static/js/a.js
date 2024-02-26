fc8f2d849279dde12193179c8c90f537102fc600
wahyu6070 <gwtsendangrejo6070@gmail.com>
2024-02-26 16:24:56 +0700

    p

--- themes/bulma/static/js/a.js
+++
@@ -1,7 +0,0 @@
-function search() {
-            var searchTerm = document.getElementById("searchTerm").value;
-            var url = "https://litegapps.site/?search=" + encodeURIComponent(searchTerm);
-            window.location.href = url;
-        }
-	
-        
